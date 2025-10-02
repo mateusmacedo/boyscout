35770febbebf6d78498d5b5f9fdc64d8f70fa891
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-10-02 00:08:59 -0300

    feat(go-api): implement hello function with proper go module structure

--- apps/go-api/main.go
+++ apps/go-api/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/mateusmacedo/boyscout/go-logger"
+	gologger "github.com/mateusmacedo/boyscout/go-logger/pkg"
+	"github.com/mateusmacedo/boyscout/go-logger/pkg/types"
 )
 
 func Hello(name string) string {
@@ -12,5 +14,35 @@ func Hello(name string) string {
 }
 
 func main() {
-	fmt.Println(gologger.GoLogger(Hello("go-api")))
+	// Configura logger
+	options := types.LogOptions{
+		Level:       types.InfoLevel,
+		Service:     "go-api",
+		Environment: "development",
+		Version:     "1.0.0",
+		Fields: map[string]interface{}{
+			"app": "go-api",
+		},
+	}
+
+	log := gologger.NewLogger(options)
+
+	// Log de inicialização
+	log.Info("Aplicação Go API iniciada", map[string]interface{}{
+		"timestamp": time.Now(),
+		"version":   "1.0.0",
+	})
+
+	// Executa função Hello com logging
+	log.Info("Executando função Hello")
+	result := Hello("go-api")
+
+	log.Info("Função Hello executada com sucesso", map[string]interface{}{
+		"result": result,
+	})
+
+	fmt.Println(result)
+
+	// Log de finalização
+	log.Info("Aplicação Go API finalizada")
 }
